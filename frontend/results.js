6c1e4d0836185ce5da5d0351eb1d5d743f9f6576
vineetsharma18 <141012912+vineetsharma18@users.noreply.github.com>
2025-06-21 22:52:17 +0530

    Create results.js
    by vineet 21 june

---
+++ frontend/results.js
@@ -0,0 +1,38 @@
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+const query = getQueryParam("query");
+const user = getQueryParam("user") || "guest";
+
+const resultsSection = document.getElementById("results");
+
+if (!query) {
+  resultsSection.innerHTML = "<p>No query provided.</p>";
+} else {
+  fetch(`http://localhost:5000/api/recommend?query=${encodeURIComponent(query)}&user=${user}`)
+    .then(res => res.json())
+    .then(data => {
+      resultsSection.innerHTML = "";
+      data.results.forEach(video => {
+        const videoId = video.link.split("v=")[1];
+        const card = document.createElement("div");
+        card.className = "video-card";
+        card.innerHTML = `
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/${videoId}" 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+          <div class="info">
+            <h3>${video.title}</h3>
+            <p>${video.channel}</p>
+          </div>
+        `;
+        resultsSection.appendChild(card);
+      });
+    })
+    .catch(err => {
+      console.error("Fetch error:", err);
+      resultsSection.innerHTML = "<p>Could not load recommendations.</p>";
+    });
+}
